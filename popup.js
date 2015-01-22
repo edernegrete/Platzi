c9b19c33db464c9c8dbe6516bbcd209dffa66eff
Ederyairr <ederyairnglz@gmail.com>
2015-01-22 17:07:12 -0600

    Add name of the actual course on the popup

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 $(function (){
 	var schedule = "https://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20html%20where%20url%3D%22http%3A%2F%2Fmejorando.la%2Fhorarios%2F%22%20and%20xpath%20%3D%20'%2F%2Fdiv%5B%40class%3D%22ScheduleDateList-date%20icon-calendar%22%5D%2Fspan'&format=json&diagnostics=true&callback=";
+	var course = "https://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20html%20where%20url%3D%22http%3A%2F%2Fmejorando.la%2Fhorarios%2F%22%20and%20xpath%20%3D%20'%2F%2Fdiv%5B%40class%3D%22ScheduleCountdown-courseInfo%22%5D%2Fspan%5B%40class%3D%22ScheduleCountdown-courseName%22%5D%2Fstrong'&format=json&diagnostics=true&callback="
 		$.getJSON(schedule,{
 			tags: "span",
 			format: "json"
@@ -14,4 +15,16 @@ $(function (){
 			}
 			resOut.html(html)
 		});
-	});
+
+		$.getJSON(course,{
+			tags: 'strong',
+			format: 'json'
+		})
+		.done(function(data){
+			var courseName = data.query.results.strong;
+			var resOut = $('#course');
+			var html = '';
+			html = courseName;
+			resOut.html(html);
+		})
+});
