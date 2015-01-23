e83a3257949ccb67a5716f534f99a3814e436722
Ederyairr <ederyairnglz@gmail.com>
2015-01-23 16:51:06 -0600

    Updated the Platzi icon to the new one

--- background.js
+++ background.js
@@ -1,21 +1,25 @@
-var className = localStorage['courseNameStorage'] ;
-var message = 'Está por comenzar la clase del '
-var opt={
-  type: 'basic',
-  title: 'Clase en Vivo',
-  message: '',
-  priority: 1,
-  iconUrl: 'icon.png'
-}
-opt['message'] = message + className;
-chrome.notifications.create('id', opt, function(id){})
-chrome.notifications.onClicked.addListener(openUrl);
-
-function openUrl () {
-	window.open("http://cursos.mejorando.la/")
-}
-Storage.prototype.getObj = function(key) {
+$(function(){
+	Storage.prototype.getObj = function(key) {
     	return JSON.parse(this.getItem(key))
-}
-var storedDays = localStorage.getObj('days');
+	}
+	var storedDays = localStorage.getObj('days');
+	console.log(storedDays)
+	var className = localStorage['courseNameStorage'] ;
+	var message = 'Está por comenzar la clase del '
+	var opt={
+	  	type: 'basic',
+	  	title: 'Clase en Vivo',
+	  	message: '',
+	  	priority: 1,
+	  	iconUrl: 'icon.png'
+	};
+	opt['message'] = message + className;
+	chrome.notifications.create('id', opt, function(id){})
+	chrome.notifications.onClicked.addListener(function(){
+		window.open("http://cursos.mejorando.la/")
+	});
+});
+
+
+
 
